da1f138474402227fbd7d4e1719b1948d9ee6f1f
Timothy Redmond <tredmond@stanford.edu>
2009-10-17 23:18:24 +0000

    added xml base logic

--- junit/org/protege/xmlcatalog/TestCatalog.java
+++ junit/org/protege/xmlcatalog/TestCatalog.java
@@ -5,14 +5,13 @@ import java.io.File;
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
-import org.protege.xmlcatalog.parser.Handler;
 
 public class TestCatalog extends TestCase {
     private static final Logger log = Logger.getLogger(TestCatalog.class);
 
     public void testExample1() {
         try {
-            Handler.parseDocument(new File("examples/Example1.xml").toURL());
+            Util.parseDocument(new File("examples/Example1.xml").toURL(), null);
         }
         catch (Throwable t) {
             log.warn("Test failed", t);
@@ -22,7 +21,7 @@ public class TestCatalog extends TestCase {
     
     public void testExample2() {
         try {
-            Handler.parseDocument(new File("examples/Example2.xml").toURL());
+            Util.parseDocument(new File("examples/Example2.xml").toURL(), null);
         }
         catch (Throwable t) {
             log.warn("Test failed", t);
