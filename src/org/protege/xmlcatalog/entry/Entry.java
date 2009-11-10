33f714f5e826c7e0bceb44773578a7198c3259e8
Timothy Redmond <tredmond@stanford.edu>
2009-11-10 01:18:16 +0000

    I am not sure that a entry is an xml base context.

--- src/org/protege/xmlcatalog/entry/Entry.java
+++ src/org/protege/xmlcatalog/entry/Entry.java
@@ -3,7 +3,7 @@ package org.protege.xmlcatalog.entry;
 import org.protege.xmlcatalog.EntryVisitor;
 import org.protege.xmlcatalog.XmlBaseContext;
 
-public abstract class Entry implements XmlBaseContext {
+public abstract class Entry {
     private String id;
     private XmlBaseContext xmlBaseContext;
 
