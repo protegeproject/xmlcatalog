cf935dc02bb864ba0959906969812c7a5783f0b5
Timothy Redmond <tredmond@stanford.edu>
2009-11-17 23:57:45 +0000

    new test comparing the apache CatalogResolver with this library.  Currently only includes uri entries.  There  are a lot of combinations that could be looked at.

--- junit/org/protege/xmlcatalog/ResolveTest.java
+++ junit/org/protege/xmlcatalog/ResolveTest.java
@@ -13,16 +13,52 @@ import org.apache.xml.resolver.CatalogManager;
 import org.apache.xml.resolver.tools.CatalogResolver;
 
 public class ResolveTest extends TestCase {
+    private CatalogResolver resolver;
+    private XMLCatalog catalog;
 
-    public void testSimple() throws TransformerException, MalformedURLException, IOException {
-        String catalogLocation = "test/catalog01.xml";
-        CatalogManager manager = new CatalogManager();
-        manager.setCatalogFiles(catalogLocation);
-        CatalogResolver resolver = new CatalogResolver(manager);
-        XMLCatalog catalog = Util.parseDocument(new File(catalogLocation).toURL(), null);
+    public void test01() throws TransformerException, MalformedURLException, IOException {
+        readCatalog("test/catalog01.xml");
+
         URI u = URI.create("http://www.tigraworld.com/protege/ontology1.owl");
         URI redirect = URI.create("file:/home/tredmond/Shared/ontologies/simple/ontology1.owl");
-        assertTrue(resolver.resolve(u.toString(), null).getSystemId().equals(redirect.toString()));
+        checkBothAlgorithmsSame(u);
         assertTrue(Util.getRedirect(u, catalog).equals(redirect));
     }
+    
+    public void test02() throws MalformedURLException, IOException, TransformerException {
+        readCatalog("test/catalog02.xml");
+
+        URI u = URI.create("http://www.tigraworld.com/protege/ontology1.owl");
+        URI redirect = URI.create("file:/home/tredmond/Shared/simple/ontology1.owl");
+        checkBothAlgorithmsSame(u);
+        assertTrue(Util.getRedirect(u, catalog).equals(redirect));
+    }
+    
+    public void test03() throws MalformedURLException, IOException, TransformerException {
+        readCatalog("test/catalog03.xml");
+
+        URI u = URI.create("http://www.tigraworld.com/protege/ontology1.owl");
+        URI redirect = new File("test/simple/ontology1.owl").toURI();
+        // this doesn't work  because of a seemingly silly problem involving the URI for the outer xml base.
+        // checkBothAlgorithmsSame(u);
+        assertTrue(Util.getRedirect(u, catalog).equals(redirect));
+    }
+    
+    private void readCatalog(String catalogLocation) throws MalformedURLException, IOException {
+        System.setProperty("xml.catalog.ignoreMissing", "true");
+        CatalogManager manager = new CatalogManager();
+        manager.setUseStaticCatalog(false);
+        manager.setCatalogFiles(catalogLocation);
+        resolver = new CatalogResolver(manager);
+        catalog = Util.parseDocument(new File(catalogLocation).toURL(), null);
+    }
+    
+    private void checkBothAlgorithmsSame(URI u) throws TransformerException {
+        String result1 = resolver.resolve(u.toString(), null).getSystemId();
+        String result2 = Util.getRedirect(u, catalog).toString();
+        assertTrue(result1.equals(result2));
+    }
+    
+
+    
 }
