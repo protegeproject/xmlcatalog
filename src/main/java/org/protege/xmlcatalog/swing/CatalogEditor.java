c9723a399c3bdcd0eca857580549405fef66dc53
Timothy Redmond <tredmond@stanford.edu>
2011-09-12 18:23:06 +0000

    imports

--- src/main/java/org/protege/xmlcatalog/swing/CatalogEditor.java
+++ src/main/java/org/protege/xmlcatalog/swing/CatalogEditor.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.BoxLayout;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
